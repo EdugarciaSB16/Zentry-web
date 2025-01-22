7527eb00b9ccd7770d716dbb8d6327ac1adfdcfa
Eduardo Garcia <edugarciasb16@gmail.com>
2025-01-22 01:05:23 +0100

    Add button prop types

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 
-const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
+const Button = ({
+    title,
+    id = '',
+    rightIcon = null,
+    leftIcon = null,
+    containerClass = '',
+}) => {
     return (
         <button
             id={id}
@@ -10,8 +16,17 @@ const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
             <span className="relative incline-flex overflow-hidden font-general text-xs uppercase">
                 <div>{title}</div>
             </span>
+            {rightIcon}
         </button>
     );
+};
+
+Button.propTypes = {
+    title: PropTypes.string.isRequired,
+    id: PropTypes.string,
+    rightIcon: PropTypes.node,
+    leftIcon: PropTypes.node,
+    containerClass: PropTypes.string,
 };
 
 export default Button;
