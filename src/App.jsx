8e8b00c6cd442b906b1f553336f92236cb7a3f20
Eduardo Garcia <edugarciasb16@gmail.com>
2025-01-24 19:12:39 +0100

    Add features to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from './components/About';
+import Features from './components/Features';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
             <Navbar />
             <Hero />
             <About />
+            <Features />
         </main>
     );
 };
